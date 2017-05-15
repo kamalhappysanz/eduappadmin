d94f51cb85c9b33250e67825eb1464027b97bfc8
kamal happysanz <kamal.happysanz@gmail.com>
2017-05-15 14:18:15 +0530

    Dashboard
    committed

--- application/views/home.php
+++ application/views/home.php
@@ -1,12 +1,69 @@
 
 	<div class="main-panel">
-
         <div class="content">
+					<div class="card">
             <div class="container-fluid">
-							<h2>Admin Dashboard</h2>
+							<h2>Admin Dashboard111</h2>
+
+							<br />
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-3">
+    	    <div class="hero-widget well well-sm">
+                <div class="icon">
+                     <i class="glyphicon glyphicon-user"></i>
+                </div>
+                <div class="text">
+                    <var>3</var>
+                    <label class="text-muted">invited guests</label>
+                </div>
+                <div class="options">
+                    <a href="javascript:;" class="btn btn-primary btn-lg"><i class="glyphicon glyphicon-plus"></i> Add a guest</a>
+                </div>
+            </div>
+		</div>
+        <div class="col-sm-3">
+            <div class="hero-widget well well-sm">
+                <div class="icon">
+                     <i class="glyphicon glyphicon-star"></i>
+                </div>
+                <div class="text">
+                    <var>614</var>
+                    <label class="text-muted">page likes</label>
+                </div>
+                <div class="options">
+                    <a href="javascript:;" class="btn btn-default btn-lg"><i class="glyphicon glyphicon-search"></i> View traffic</a>
+                </div>
+            </div>
+		</div>
+        <div class="col-sm-3">
+            <div class="hero-widget well well-sm">
+                <div class="icon">
+                     <i class="glyphicon glyphicon-tags"></i>
+                </div>
+                <div class="text">
+                    <var>73</var>
+                    <label class="text-muted">total orders</label>
+                </div>
+                <div class="options">
+                    <a href="javascript:;" class="btn btn-default btn-lg"><i class="glyphicon glyphicon-search"></i> View orders</a>
+                </div>
+            </div>
+    	</div>
+
+	</div>
 </div>
 
 
+						</div>
 
-
-    </div>
+				</div>
+			</div>
+<style>
+.hero-widget { text-align: center; padding-top: 20px; padding-bottom: 20px; }
+.hero-widget .icon { display: block; font-size: 96px; line-height: 96px; margin-bottom: 10px; text-align: center; }
+.hero-widget var { display: block; height: 64px; font-size: 64px; line-height: 64px; font-style: normal; }
+.hero-widget label { font-size: 17px; }
+.hero-widget .options { margin-top: 10px; }
+</style>
