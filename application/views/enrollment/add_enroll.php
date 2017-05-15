9aaa5c50a0e090839a7b37f1561009eb5fffc818
devika5595 <devika.happysanz@gmail.com>
2017-05-15 14:22:32 +0530

    readonly changes
    commited

--- application/views/enrollment/add_enroll.php
+++ application/views/enrollment/add_enroll.php
@@ -53,7 +53,7 @@
                                         <div class="form-group">
                                           <label class="col-sm-2 control-label">Admission No</label>
                                           <div class="col-sm-4">
-            <input type="text" class="form-control" value="<?php echo $rows->admisn_no; ?>" name="admisn_no" id="admission_no">
+            <input type="text" class="form-control" readonly value="<?php echo $rows->admisn_no; ?>" name="admisn_no" id="admission_no">
 				  <input type="hidden" class="form-control" value="<?php echo $rows->admission_id; ?>" name="admission_id" id="admission_id">
 
                                           </div>
